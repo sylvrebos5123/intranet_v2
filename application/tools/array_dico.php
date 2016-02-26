080c1aa48254ee2c22468d8c9b70350f0e22412a
vrebos.sylvie <sylvievrebos@gmail.com>
2016-02-26 17:16:12 +0100

    adding timezone grid

--- application/tools/array_dico.php
+++ application/tools/array_dico.php
@@ -3,7 +3,7 @@
 /* fichier généré par dico_generate_array.php  */ 
 /* sur base des informations contenues dans la */ 
 /* table `cpas_dico` dans la DB                */ 
-/* génére le : 2016-02-19 02:19:40             */ 
+/* génére le : 2016-02-26 02:47:27             */ 
 /***********************************************/ 
 /* les modifications manuelles effectuées dans */ 
 /* ce fichier seront écrasés lors de           */ 
--- application/views/calendrier/agenda_officiel.php
+++ application/views/calendrier/agenda_officiel.php
@@ -16,10 +16,6 @@
 	</div>
 </div>
 
-<?php
-//echo $_SESSION['langue'];
-
-?>
 	
 
 <div id='calendar'></div>
@@ -27,6 +23,105 @@
 <script>
 
 	$(document).ready(function() {
+		var currentTimezone = false;
+
+		// when the timezone selector changes, rerender the calendar
+		$('#timezone-selector').on('change', function() {
+			currentTimezone = this.value || false;
+			$('#calendar').fullCalendar('destroy');
+			renderCalendar();
+		});
+
+		function renderCalendar() {
+			$('#calendar').fullCalendar({
+				header: {
+					left: 'prev,next today',
+					center: 'title',
+					right: 'month,agendaWeek,agendaDay'
+				},
+				defaultDate: '<?php echo date('Y-m-d');?>',
+				timezone: currentTimezone,
+				editable: true,
+				selectable: true,
+				eventLimit: true, // allow "more" link when too many events
+				timeFormat: 'H:mm', // uppercase H for 24-hour clock
+				events: [
+					<?php	
+				foreach($a_rdv as $k=>$v) //result data
+				{
+					$start_date=explode('/',$v['START_DATE']);
+					$end_date=explode('/',$v['END_DATE']);
+					if($v['OU']=='')
+					{
+						$ou='';
+					}
+					else
+					{
+						$ou=' - '.$v['OU'];
+					}
+					
+					//start_heure
+					if(empty($v['START_HEURE']))
+					{
+						$start_heure='';
+					}
+					else
+					{
+						$start_heure='T'.$v['START_HEURE'];
+					
+					}
+					//end_heure
+					if(empty($v['END_HEURE']))
+					{
+						$end_heure='';
+					}
+					else
+					{
+						$end_heure='T'.$v['END_HEURE'];
+					
+					}
+			?>
+					{
+						title: '<?php echo $v['SUJET'].$ou;?>',
+						start: '<?php echo $start_date[2].'-'.$start_date[1].'-'.$start_date[0].$start_heure;?>',
+						end: '<?php echo $end_date[2].'-'.$end_date[1].'-'.$end_date[0].$end_heure;?>'
+					},
+				
+			<?php
+				}
+			?>
+					{
+						title: 'All Day Event',
+						start: '0000-00-00'
+					}
+
+
+				],
+				loading: function(bool) {
+					$('#loading').toggle(bool);
+				},
+				eventRender: function(event, el) {
+					// render the timezone offset below the event title
+					if (event.start.hasZone()) {
+						el.find('.fc-title').after(
+							$('<div class="tzo"/>').text(event.start.format('Z'))
+						);
+					}
+				},
+				dayClick: function(date) {
+					console.log('dayClick', date.format());
+				},
+				select: function(startDate, endDate) {
+					console.log('select', startDate.format(), endDate.format());
+				}
+			});
+		}
+
+		renderCalendar();
+	});
+
+
+	/*$(document).ready(function() {
 		
 		$('#calendar').fullCalendar({
 			
@@ -36,6 +131,10 @@
 				right: 'month,basicWeek,basicDay'
 			},
 			defaultDate: '<?php echo date('Y-m-d');?>',
+			defaultView: 'basicWeek',
+			timezone: 'UTC',
+			ignoreTimezone: false,
+
 			/*buttonText :
 			{
 			today:    '<?php echo dico('today',$_SESSION['langue']);?>',
@@ -49,8 +148,9 @@
 			dayNamesShort:
 			['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
 			*/
-			editable: false,
+			/*editable: false,
 			eventLimit: true, // allow "more" link when too many events
+			
 			events: [
 			
 			<?php	
@@ -107,7 +207,7 @@
 			
 		});
 		
-	});
+	});*/
 
 </script>
 
