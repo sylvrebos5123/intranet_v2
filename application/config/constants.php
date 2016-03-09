cccb07fda8a83f2c8195ca9fcfe203548ddea155
vrebos.sylvie <sylvievrebos@gmail.com>
2016-03-09 14:21:41 +0100

    adding ROOT

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+define('ROOT', "http://testweb.cpasixelles.be");
 /*
 |--------------------------------------------------------------------------
 | Display Debug backtrace
